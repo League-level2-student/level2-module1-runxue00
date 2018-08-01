218f57c458ac0626cc3accf39cc7909c18cbc4ca
runxue00 <manybacks.stefanie@yahoo.com>
2018-08-01 15:02:34 -0700

    LeagueInvaders(IP)

--- src/intro_to_array_lists/ObjectManager.java
+++ src/intro_to_array_lists/ObjectManager.java
@@ -1,5 +1,25 @@
 package intro_to_array_lists;
 
-public class ObjectManager {
+import java.awt.*;
 
+public class ObjectManager {
+Rocketship rocket;
+
+public ObjectManager(Rocketship rocket) {
+	this.rocket = rocket;
 }
+
+public void update() {
+	rocket.update();
+}
+public void draw(Graphics g) {
+	
+}
+}
+
+
+
+
+
+
+// PART 9 STEP 7
